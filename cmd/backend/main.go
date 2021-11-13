be4333daa247b99201901216afc56fda38188b7c
Mihail Chapenko <mihail.chapenko@gmail.com>
2021-11-13 21:08:52 +0200

    added hiroku version

--- cmd/backend/main.go
+++ cmd/backend/main.go
@@ -5,12 +5,16 @@ import (
 	"github.com/MihailChapenko/chat/pkg/openapi3"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	port := "8080"
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // Default port if not specified
+	}
 	h := openapi3.Handler(handlers.ChatServer{})
 
 	log.Println("Starting server on port :8080")
-	http.ListenAndServe(process.env.PORT || port, h)
+	http.ListenAndServe(":"+port, h)
 }
